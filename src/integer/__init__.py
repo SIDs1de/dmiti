33252cd06a2895b9045727a6a8d103f5e2e8c0ac
infreyr <lactochka2010@gmail.com>
2025-11-06 18:41:54 +0300

    feat: новый модуль mul_zm_z

--- src/integer/__init__.py
+++ src/integer/__init__.py
@@ -1,11 +1,13 @@
 from .base_integer import BaseInteger
 from .abs_z_n import Abs_z_n
 from .poz_z_d import Poz_z_d
+from .mul_zm_z import Mul_zm_z
 
 class Integer(
     BaseInteger,
     Abs_z_n,
-    Poz_z_d
+    Poz_z_d,
+    Mul_zm_z
 ):
     """Класс целого числа"""
     pass
