37f4c8601bc641498598caa36aa9ac2baa82eeac
Карпов Андрей <andreyka527@yandex.ru>
2025-11-06 19:30:21 +0300

    feat: добавлен метод умножения натурального числа на 10^k

--- src/natural/__init__.py
+++ src/natural/__init__.py
@@ -1,12 +1,15 @@
 from .base import BaseNatural
 from .com_nn_d import COM_NN_D
 from .mul_nd_n import MUL_ND_N 
+from .mul_nk_n import MUL_Nk_N 
 from .nzer_n_b import NZER_N_B
+
 
 class Natural(
     BaseNatural,
     COM_NN_D,
     MUL_ND_N,
+    MUL_Nk_N,
     NZER_N_B
 ):
     """Класс натурального числа"""
