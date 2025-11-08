40df8186de14a55bc56e6439ae325e94f4c72394
vienees <iliyadubrovskij@gmail.com>
2025-11-08 15:27:53 +0300

    feat: добавлен метод Trans_z_n и тесты

--- src/integer/__init__.py
+++ src/integer/__init__.py
@@ -2,12 +2,14 @@ from .base_integer import BaseInteger
 from .abs_z_n import Abs_z_n
 from .poz_z_d import Poz_z_d
 from .mul_zm_z import Mul_zm_z
+from .trans_z_n import Trans_z_n
 
 class Integer(
     BaseInteger,
     Abs_z_n,
     Poz_z_d,
-    Mul_zm_z
+    Mul_zm_z,
+    Trans_z_n
 ):
     """Класс целого числа"""
     pass
