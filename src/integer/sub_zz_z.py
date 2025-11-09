44fa4c1694d40e4781cac09edb53dbad99292dfa
vapermitin_24 <dz_vitya@rambler.ru>
2025-11-09 15:21:47 +0300

    feat: sub_zz_z

---
+++ src/integer/sub_zz_z.py
@@ -0,0 +1,10 @@
+from typing import Self
+
+
+class Sub_zz_z:
+    def sub_zz_z(self: Self, second: Self) -> Self:
+        """
+        Приготовил: Viktor Permitin
+        Вычитание целых чисел
+        """
+        return self.add_zz_z(second.mul_zm_z())
