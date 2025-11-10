72c80a55027ab015590b340b3e0e8bfa1335e096
alemminik <egorsvincov@vk.com>
2025-11-10 04:21:35 +0300

    feat: add_qq_q: done

--- src/rational/__init__.py
+++ src/rational/__init__.py
@@ -1,9 +1,11 @@
 from .base_rational import BaseRational
 from .mul_qq_q  import MUL_QQ_Q
+from .add_qq_q import ADD_QQ_Q
 
 class Rational(
     BaseRational,
-    MUL_QQ_Q
+    MUL_QQ_Q,
+    ADD_QQ_Q
 ):
 
     pass
