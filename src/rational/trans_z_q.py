365dfc983e194675c6cc2627e39c6af83911c0fe
vienees <iliyadubrovskij@gmail.com>
2025-11-11 04:53:40 +0300

    fix:исправлены natural и тп

--- src/rational/trans_z_q.py
+++ src/rational/trans_z_q.py
@@ -1,15 +1,14 @@
-from src.integer import BaseInteger
-from src.natural import BaseNatural
-from src.rational import BaseRational
+from src.integer import Integer
+from src.natural import Natural
 from typing import Self
 
 class TRANS_Z_Q:
-    def TRANS_Z_Q(self, integer: BaseInteger) -> BaseRational:
+    def TRANS_Z_Q(self: Self, integer: Integer) -> Self:
         """
         Преобразование целого в дробное
         
         Автор: Дубровский Илья гр.4383
         """
         # Целое число преобразуется в дробь с знаменателем 1
-        denominator = BaseNatural([1])
-        return BaseRational(integer, denominator)
+        denominator = Natural([1])
+        return self.__class__(integer, denominator)
