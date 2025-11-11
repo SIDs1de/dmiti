3a6724be923cc0f2cecc4a552448760592046df8
vienees <iliyadubrovskij@gmail.com>
2025-11-11 05:03:28 +0300

    feat: добавлен метод TRANS_Q_Z

--- src/rational/__init__.py
+++ src/rational/__init__.py
@@ -4,14 +4,17 @@ from .int_q_b import INT_Q_B
 from .add_qq_q import ADD_QQ_Q
 from .sub_qq_q import SUB_QQ_Q
 from .red_q_q import RED_Q_Q
+from .trans_q_z import TRANS_Q_Z
+
 
 class Rational(
     BaseRational,
     MUL_QQ_Q,
-    INT_Q_B
+    INT_Q_B,
     ADD_QQ_Q,
     SUB_QQ_Q,
-    RED_Q_Q
+    RED_Q_Q,
+    TRANS_Q_Z
 ):
 
     pass
