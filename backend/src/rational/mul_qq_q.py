a08f465de3f037e00141f02740e05966276498f7
Jab1ko <rubanegoo@gmail.com>
2025-11-25 15:03:42 +0300

    fix/comments

--- backend/src/rational/mul_qq_q.py
+++ backend/src/rational/mul_qq_q.py
@@ -11,11 +11,18 @@ class MUL_QQ_Q:
         Автор: Рубан Егор гр.4383
         """
 
+        # Умножаем числители двух рациональных чисел
+        # mul_zz_z выполняет умножение целых чисел и возвращает целое число
         numerator = self.numerator.mul_zz_z(second.numerator)
 
+        # Умножаем знаменатели
+        # MUL_NN_N выполняет умножение натуральных чисел
         denominator = self.denominator.MUL_NN_N(second.denominator)
 
+        # Формируем новое рациональное число из полученного числителя и знаменателя
         result = self.__class__(numerator, denominator)
 
+        # Сокращаем результат
+        # RED_Q_Q приводит рациональное число к несократимому виду
         return result.RED_Q_Q()
 
